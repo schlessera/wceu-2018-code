b542b1682ad69e67abc434492a0de0c12a11a460
Alain Schlesser <alain.schlesser@gmail.com>
2018-06-06 12:33:50 +0200

    Add proper header to remote request

--- src/SocialNetwork/WordPress/Client.php
+++ src/SocialNetwork/WordPress/Client.php
@@ -15,18 +15,25 @@ final class Client {
 	 * @return array Array of mentions for the given user.
 	 */
 	public function get_feed( string $mention, int $limit ) {
-		$endpoint = '/read/search';
+		$endpoint   = '/read/search';
 		$parameters = [
 			'q'      => $mention,
 			'number' => $limit,
 		];
+		$headers    = [ 'Content-Type' => 'application/json' ];
 
 		$url = self::URL . $endpoint . '?' . http_build_query( $parameters );
 
-		$response = wp_remote_get( $url );
-		$code = wp_remote_retrieve_response_code( $response );
+		$args = [
+			'timeout'     => 2,
+			'httpversion' => '1.1',
+			'headers'     => $headers,
+		];
 
-		if ( $code != 200 ) {
+		$response = wp_remote_get( $url, $args );
+		$code     = wp_remote_retrieve_response_code( $response );
+
+		if ( (int) $code !== 200 ) {
 			// The WordPress.com API produced an error, so just return an empty array instead
 			return [];
 		}

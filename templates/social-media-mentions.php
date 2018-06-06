1cf222e192be76b7a16f06e8592d441fd78a7fc8
Alain Schlesser <alain.schlesser@gmail.com>
2018-06-06 13:02:40 +0200

    Fine-tune rendering

--- templates/social-media-mentions.php
+++ templates/social-media-mentions.php
@@ -3,7 +3,7 @@
 		<div class="media">
 			<img src="<?= $entry->get_avatar_image_url() ?>" class="media--img">
 			<div class="media--bd">
-				<p class="media--header"><span class="author-name"><?= $entry->get_author_name() ?></span> <span class="author-slug"><?= $entry->get_author_slug() ?></span> <span class="time"><?= $this->date_formatter->format( $entry->get_posted_time() ) ?></span></p>
+				<p class="media--header"><span class="author-name"><?= $entry->get_author_name() ?></span> <span class="author-slug"><?= $entry->get_author_slug() ?></span> &middot; <span class="time"><?= $this->date_formatter->format( $entry->get_posted_time() ) ?></span></p>
 				<p class="media--content"><?= $entry->get_content() ?></p>
 			</div>
 		</div>

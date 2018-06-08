7e336d8f9e441b2b5aba1506310a3cd8a1fd5044
Alain Schlesser <alain.schlesser@gmail.com>
2018-06-08 18:39:20 +0200

    Add Twitter credentials instruction

--- config/twitter-credentials.php
+++ config/twitter-credentials.php
@@ -7,6 +7,9 @@ namespace WordCampEurope\Workshop\Config;
  *
  * The credentials can not only be encoded as default values in this file (which
  * is NOT recommended), but can also be provided through environment variables.
+ *
+ * You can create an application for the Twitter API and retrieve its
+ * authentication keys here: https://apps.twitter.com/
  */
 
 return [
